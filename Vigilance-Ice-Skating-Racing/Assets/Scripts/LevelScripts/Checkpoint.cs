73def5ad0f6e52ca9c2ce2a85406b2fbb2c3ca5e
Laurence Sadler <sirlaurence@protonmail.com>
2021-04-21 14:10:15 -0700

    [ADDED] Checkpoint scripts

--- Vigilance-Ice-Skating-Racing/Assets/Scripts/LevelScripts/Checkpoint.cs
+++ Vigilance-Ice-Skating-Racing/Assets/Scripts/LevelScripts/Checkpoint.cs
@@ -5,8 +5,10 @@ namespace LevelScripts
 {
 	public class Checkpoint : MonoBehaviour
 	{
-		private void OnTriggerEnter(Collider other) {
-			// 
+		private EndlessLevel endlessLevelData;
+		private void OnTriggerEnter2D(Collider2D other) {
+			endlessLevelData = FindObjectOfType<EndlessLevel>();
+			endlessLevelData.RespawnLocation = (Vector2) gameObject.transform.position;
 		}
 	}
 }
